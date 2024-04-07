7ca92fb0c17a743e4f90a434af7219f7c20f85da
kevin <kevin-hamza.djeradi@epitech.eu>
2024-04-07 23:59:35 +0200

    created main.dart

---
+++ kcall_frontend/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'video_call_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'K Call',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const VideoCallScreen(),
+    );
+  }
+}
